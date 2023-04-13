e28e5b6cb9dcadc0fe0a631f7cac2b2d8f419d77
Wongboo <Wongbooo@outlook.com>
2023-04-13 21:21:09 +0800

    加入作业一代吗

---
+++ proj1mk/test_bp.py
@@ -0,0 +1,53 @@
+# function Test_BP
+
+# min mu*||x||_1 + ||Ax-b||_1
+
+import time
+import numpy as np
+import scipy.sparse as sp
+from numpy import norm, rand, randn
+
+
+def sprandn(m, n, density):
+    return sp.random(m, n, density=density, format='csc',
+                     data_rvs=lambda s: np.random.normal(size=s))
+
+
+# generate data
+n = 1024
+m = 512
+A = randn(m, n)
+u = sprandn(n, 1, 0.1)
+b = A*u
+mu = 1e-2
+x0 = rand(n, 1)
+
+
+def errfun(x1, x2): return norm(x1-x2)/(1+norm(x1))
+def resfun(x): return norm(A@x-b, 1)
+def nrm1fun(x): return norm(x, 1)
+
+
+# cvxpy calling mosek
+opts1 = {}  # modify options
+start_time = time.time()
+x1, out1 = l1_cvx_mosek(x0, A, b, mu, opts1)
+t1 = time.time() - start_time
+
+# direct calling gurobi
+opts2 = opts_gurobi()  # modify options
+start_time = time.time()
+x2, out2 = l1_gurobi(x0, A, b, mu, opts2)
+t2 = time.time() - start_time
+
+# admm
+opts5 = opts_admm()  # modify options
+start_time = time.time()
+x5, out5 = l1_admm(x0, A, b, mu, opts5)
+t5 = time.time() - start_time
+
+
+# print comparison results with cvx-call-mosek
+print(f'cvxpy-mosek:  res: {resfun(x1):3.2e}, nrm1: {nrm1fun(x1):3.2e}, cpu: {t1:5.2f}')    # noqa:E501
+print(f'gurobi:       res: {resfun(x2):3.2e}, nrm1: {nrm1fun(x2):3.2e}, cpu: {t2:5.2f}, err-to-cvxpy-mosek: {errfun(x1, x2):3.2e}')  # noqa:E501
+print(f'admm:         res: {resfun(x5):3.2e}, nrm1: {nrm1fun(x5):3.2e}, cpu: {t5:5.2f}, err-to-cvxpy-mosek: {errfun(x1, x5):3.2e}')  # noqa:E501
---
+++ proj1mk/test_mc.py
@@ -0,0 +1,31 @@
+# function Test_MC
+
+# min mu*||X||_* + sum_{ij in idx}
+
+import numpy as np
+from numpy import round, floor, sqrt, randn
+
+
+# generate data
+m = 40; n = 40; sr = 0.3; p = int(round(m*n*sr)); r = 3  # noqa: E702
+# m = 100; n = m; sr = 0.3; p = int(round(m*n*sr)); r = 2  # noqa: E702
+# m = 100; n = m ; p = 5666; sr = p/m/n; r = 10  # noqa: E702
+# m = 200; n = m; p = 15665; sr = p/m/n; r = 10  # noqa: E702
+# m = 500; n = m; p = 49471; sr = p/m/n; r = 10  # noqa: E702
+# m = 150; n = 300; sr = 0.49; p = int(round(m*n*sr)); r = 10  # noqa: E702
+
+
+# fr is the freedom of set of rank-r matrix, maxr is the maximum rank one
+# can recover with p samples, which is the max rank to keep fr < 1
+fr = r*(m+n-r)/p
+maxr = floor(((m+n)-sqrt((m+n)**2-4*p))/2)
+
+rs = 2021
+np.random.seed(rs)
+
+# get problem
+omega = np.random.permutation(m*n)[:p]  # Omega gives the position of samplings
+xl = randn(m, r)
+xr = randn(n, r)
+a = xl @ xr.T  # A is the matrix to be completed
+m_mat = a.reshape(-1)[omega]  # M is the samples from A
